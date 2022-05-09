60b95b1cd88cff11524a5f3b29ed1cb9cbf98ffa
Bourek, Daniel <bouredan@fel.cvut.cz>
2022-05-10 01:04:31 +0200

    add cm to px, add vzor_01.jpg

--- main.py
+++ main.py
@@ -1,16 +1,23 @@
 import sys
+import math
+
 from PIL import Image
 
 
-def colored(r, g, b, text):
+def colored(r: tuple, g: tuple, b: tuple, text: list):
     return "\033[48;2;{};{};{}m{} \u001b[0m".format(r, g, b, text)
 
 
-def pixels_equal(pixel1, pixel2, tolerance):
+def pixels_equal(pixel1: tuple, pixel2: tuple, tolerance: int):
     for i in range(len(pixel1)):
         if abs(pixel1[i] - pixel2[i]) > tolerance:
             return False
     return True
+
+
+def print_color_part(distance: int, color_rgb: list, px_to_cm: int):
+    distance_cm = math.ceil(distance / px_to_cm)
+    print("Barva ", colored(color_rgb[0], color_rgb[1], color_rgb[2], color_rgb), " na ", distance_cm, "cm.")
 
 
 if len(sys.argv) < 2:
@@ -22,10 +29,11 @@ with Image.open(sys.argv[1]) as im:
     width, height = im.size
     pixels = [pixels[i * width:(i + 1) * width] for i in range(height)]
     print("Vzor s rozměry ", width, "x", height, "px")
+    px_to_cm_input = int(input("Kolik px chcete reprezentovat jako 1cm? "))
 
     for index, pixelRow in enumerate(pixels):
         is_even = index % 2 == 0
-        print("Řádek č. ", index, " zleva doprava" if is_even else "zprava doleva")
+        print("Řádek č.", index, "zleva doprava" if is_even else "zprava doleva")
         print("-------------------------------------------")
         last_distance = 0
         last_pixel = pixelRow[0]
@@ -33,8 +41,8 @@ with Image.open(sys.argv[1]) as im:
             if pixels_equal(pixel, last_pixel, 30):
                 last_distance += 1
             else:
-                print("Barva ", colored(last_pixel[0], last_pixel[1], last_pixel[2], last_pixel), " na ", last_distance,
-                      "px.")
+                print_color_part(last_distance, last_pixel, px_to_cm_input)
                 last_pixel = pixel
                 last_distance = 1
-        print("Barva ", colored(last_pixel[0], last_pixel[1], last_pixel[2], last_pixel), " na ", last_distance, "px.\n\n")
+        print_color_part(last_distance, last_pixel, px_to_cm_input)
+        print("\n")
